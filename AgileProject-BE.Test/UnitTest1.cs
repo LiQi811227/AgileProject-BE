f2a448d9a098b83f5468dd183cbd0d3f22201965
LiQi811227 <qis06@myunitec.ac.nz>
2019-09-16 12:20:14 +0800

    initial commit

---
+++ AgileProject-BE.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using AgileProject_BE.Controllers;
+
+namespace AgileProject_BE.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+    }
+}
