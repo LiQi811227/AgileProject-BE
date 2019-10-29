a81771ede110952fcc3c36d640438f4b30d9c45e
LiQi811227 <54206045+LiQi811227@users.noreply.github.com>
2019-10-30 11:04:08 +1300

    Update for automatic testing

--- AgileProject-BE.Test/UnitTest1.cs
+++ AgileProject-BE.Test/UnitTest1.cs
@@ -14,5 +14,103 @@ namespace AgileProject_BE.Test
             var returnValue = controller.Get(1);
             Assert.Equal("liqi", returnValue.ToString());
         }
+        
+        [Fact]
+        public void GetReturnsMyName2()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName3()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName4()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName5()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName6()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName7()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName8()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName9()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName10()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName11()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName12()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName13()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName14()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
+        
+        [Fact]
+        public void GetReturnsMyName15()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("liqi", returnValue.ToString());
+        }
     }
 }
